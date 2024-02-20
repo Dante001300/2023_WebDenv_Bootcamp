9cdb7f7a2386a5b492759395894bc2cb9aeef2ea
Dante001300 <tdante95@gmail.com>
2024-02-19 19:58:10 -0500

    BS

---
+++ 79 Javascript Variables Exercise Start/index.js
@@ -0,0 +1,8 @@
+// very important go to sources --> then select snippets --> create file index.js --> then clikc ctrl + enter to run
+
+
+
+
+var character = prompt("type in something");
+var character_count = character.length;
+alert("you have written " + character_count + (" characters, and you have remaining " + (140-character_count) + " chacters"));
---
+++ 83 String Lengths and Retrieving the Number of Characters/index.js
@@ -0,0 +1,3 @@
+var character = promt("type something");
+var character_count = character.length;
+alert ("you have written" + character_count + "characters, you have" + (140 - character_count) + "character remaining" );
