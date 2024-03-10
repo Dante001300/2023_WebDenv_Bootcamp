7e987b15495a49af79f20549a5255bb967bf5c3d
Dante001300 <tdante95@gmail.com>
2024-03-10 17:43:45 -0400

    83 and 84

--- 83 String Lengths and Retrieving the Number of Characters/index.js
+++ 83 String Lengths and Retrieving the Number of Characters/index.js
@@ -1,3 +1,7 @@
+// very important go to sources --> then select snippets --> create file index.js --> then clikc ctrl + enter to run
+// the goal of this exercise is to count the number of character the user has typed and let the user
+// know how many character is remaining.
+
 var character = promt("type something");
 var character_count = character.length;
 alert ("you have written" + character_count + "characters, you have" + (140 - character_count) + "character remaining" );
---
+++ 84 Slicing and Extracting Parts of a String/index.js
@@ -0,0 +1,12 @@
+// <!-- very important go to sources then select snippets create file index.js -> then clikc ctrl + enter to run ->
+// In this lession we'll learn syntax slice it's a way to cut off the string
+// for example:    var name = "Dante"
+//                 name.slice(0.1) // this means starting at position 0 and stop at but not include position 1. it should result in first letter D 
+
+// for example:    var name = "Dante"
+//                 name.slice(0.3) // start at position 0 and stop at but not include position 3 should result in Dan -> -> -->
+
+var char = prompt("type in something");
+var char_140 = char.slice(0.140);
+alert(char_140);
+
