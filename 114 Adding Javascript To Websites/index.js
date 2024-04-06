ba45b75900120466ef6a7ad530a7214a4137e995
Dante001300 <tdante95@gmail.com>
2024-04-06 16:34:03 -0400

    114

---
+++ 114 Adding Javascript To Websites/index.js
@@ -0,0 +1,10 @@
+/*
+There are 3 way to incoporate Javascript into HTML
+1. In line  -- adding javascript directly into the line of code
+
+2. Internal -- use --------------->  <script type="text/javascript"> </script>
+
+3. External -- use an external javascript file ------------>          <script src="index.js" charset="utf-8"> </script>
+
+position of javascript matter just like css where we place css just below the title.
+*/
