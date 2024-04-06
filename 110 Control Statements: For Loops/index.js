f4d58ec0ad12843b79d01ec397fb5597803a3d6c
Dante001300 <tdante95@gmail.com>
2024-04-06 16:13:10 -0400

    112 FIbonacci VIP

---
+++ 110 Control Statements: For Loops/index.js
@@ -0,0 +1,10 @@
+// for loops --> for (i=0; i<2; i++){}   // for (star; end; change)
+
+for (var i=0; i<2; i++){
+    console.log(i);
+}
+
+
+while loop is checking the state true or false
+
+ for loop is iterate how many time you want the function to do something.
