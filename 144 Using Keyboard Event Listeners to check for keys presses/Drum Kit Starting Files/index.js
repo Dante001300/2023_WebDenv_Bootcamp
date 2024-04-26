c7ea169ae423ed578558bcc3c85e0958b6310fb2
Dante001300 <tdante95@gmail.com>
2024-04-25 22:00:42 -0400

    key press down

---
+++ 144 Using Keyboard Event Listeners to check for keys presses/Drum Kit Starting Files/index.js
@@ -0,0 +1,80 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event this link is for keydown
+
+//-------------------------------using key down to detect keyboard presses-------------------------------------------------]
+
+
+var numberOfDrum = document.querySelectorAll(".drum").length;
+//====================================This is the for function to go through all the picture===========and button got click
+
+for(var i = 0; i < numberOfDrum; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;  //thi.innerHTML converts the javascript into html string
+        toMakeSound(buttonInnerHTML); //This will allow the function toMakeSound to use the variable buttonInnerHTML to run click on button
+    });
+
+}
+//======================================This is when key is pressed down===================================================
+
+    document.addEventListener("keydown", function(dante){
+        dante = dante.key; // key here is similar to this.innerHTML it conver the javscript into html string
+        toMakeSound(dante);
+    });
+
+//=============================================this is the switch statement to make sound==================================
+
+function toMakeSound(key){
+
+        switch (key) {
+            case "w":
+                var w_drum = new Audio("./sounds/crash.mp3");
+                w_drum.play();
+                break;
+    
+            case "a":
+                var a_drum = new Audio("./sounds/kick-bass.mp3");
+                a_drum.play();
+                break;
+
+            case "s":
+                var s_drum = new Audio("./sounds/snare.mp3");
+                s_drum.play();
+                break;
+
+            case "d":
+                var d_drum = new Audio("./sounds/tom-1.mp3");
+                d_drum.play();
+                break;
+
+            case "j":
+                var j_drum = new Audio("./sounds/tom-2.mp3");
+                j_drum.play();
+                break;
+
+            case "k":
+                var k_drum = new Audio("./sounds/tom-3.mp3");
+                k_drum.play();
+                break;
+
+            case "l":
+                var l_drum = new Audio("./sounds/tom-4.mp3");
+                l_drum.play();
+                break;
+        
+            default:
+                console.log("nothing");
+                break;
+        }
+}
+
+
+//===================================================to detect key pressed========================================
+// document.addEventListener("keydown", iPressed); // "keydown" here is a key syntax
+
+// // we can input a variable inside the function so it can return the value back
+// function iPressed (dante){
+//     var dante = dante.key;
+// //so for key board press we have to use .key to convert to text (this is simlar to --> var drumType = this.innerHTML;)
+//     console.log(dante);
+// }
+
